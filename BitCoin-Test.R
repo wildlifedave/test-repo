0fb045409ddefe1765089f69940d801abbd03991
wildlifedave <davidmichaelkrol@gmail.com>
2017-11-26 23:41:04 -0500

    Add files via upload

---
+++ BitCoin-Test.R
@@ -0,0 +1,104 @@
+# PRICING
+OpenRaw <- raw[c(1,2)] # Choose Open Price for that day
+HighRaw <- raw[c(1,3)] # Choose High Price for that day
+LowRaw <- raw[c(1,4)] # Choose Low Price for that day
+CloseRaw <- raw[c(1,5)] # Choose Close Price for that day
+# PRICING
+
+# CALCULATE ALL PRICE DIFFERENCES
+BuyPrice <- OpenRaw[, 2]
+SellPrice <- OpenRaw[, 2]
+BuyPrice <- BuyPrice[['Open']]
+SellPrice <- SellPrice[['Open']]
+BuyPrice <- rev(BuyPrice)
+SellPrice <- rev(SellPrice)
+FinalPriceTest <- c()
+Tmp <- c()
+
+pb <- progress_bar$new(
+  format = "  downloading [:bar] :percent in :elapsed",
+  total = 100, clear = FALSE, width= 60)  
+
+for(i in 1:1552) {
+  pb$tick()
+  Sys.sleep(1 / 100)
+    Tmp <- SellPrice[i:1552]-BuyPrice[i]
+    FinalPriceTest <- c(FinalPriceTest, Tmp)
+  }
+# CALCULATE ALL PRICE DIFFERENCES
+
+
+# BLANK DATE VECTORS
+FinalBuy <- c()
+TempBuy <-c()
+FinalSell <- c()
+TempSell <- c()
+# BLANK DATE VECTORS
+
+# BUILDING FINAL BUY DATE LIST
+BuyDateList <- OpenRaw[, 1]
+BuyDateList <- BuyDateList[['Date']]
+BuyDateList <- rev(BuyDateList)
+
+
+pb <- progress_bar$new(
+  format = "  downloading [:bar] :percent in :elapsed",
+   total = 100, clear = FALSE, width= 60)
+
+for(i in 1:1552) {
+  pb$tick()
+  Sys.sleep(1 / 100)
+TempBuy <- rep(BuyDateList[i], 1553-i)
+FinalBuy <- c(FinalBuy, TempBuy)
+}
+# BUILDING FINAL BUY DATE LIST
+
+# BUILDING FINAL SELL DATE LIST
+SellDateList <- BuyDateList
+
+pb <- progress_bar$new(
+  format = "  downloading [:bar] :percent eta: :eta",
+  total = 100, clear = FALSE, width= 60)
+
+pb$tick()
+  Sys.sleep(1 / 100)
+    for(i in 1:1552) {
+      TempSell <- SellDateList[i:1552]
+      FinalSell <- c(FinalSell, TempSell)
+    }
+# BUILDING FINAL SELL DATE LIST
+
+# CALCULATING INTERVAL DAYS
+ Days <- c()
+ TempDays <- c()
+ DaysList <- 0:1551
+ Days <- DaysList
+   pb <- progress_bar$new(
+    format = "  downloading [:bar] :percent eta: :eta",
+    total = 100, clear = FALSE, width= 60)
+  
+  pb$tick()
+  Sys.sleep(1 / 100)
+  for(i in 1:1552) {
+    TempDays <- head(DaysList,-i)
+    Days <- c(Days, TempDays)
+  }
+# CALCULATING INTERVAL DAYS  
+  
+# PUTTING COLUMNS TOGETHER WITH LABELS
+FinalTable <- data.frame()
+pb <- progress_bar$new(
+  format = "  downloading [:bar] :percent eta: :eta",
+  total = 100, clear = FALSE, width= 60)
+
+pb$tick()
+Sys.sleep(1 / 100)
+FinalTable <- cbind(FinalBuy, FinalSell, Days, FinalPriceTest)
+colnames(FinalTable) <- c("BuyDate", "SellDate", "Days", "PriceDiff")
+# PUTTING COLUMNS TOGETHER WITH LABELS
+
+# DATA CLASS & TYPE MANIPULATIONS
+FinalTable <- as.data.frame(FinalTable)
+PriceVector <- FinalTable[[4]]
+DaysVector <- FinalTable[[3]]
+# DATA CLASS & TYPE MANIPULATIONS
